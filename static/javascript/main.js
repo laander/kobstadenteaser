0b2577b5a2b8c2aa4f833dc2de66e1a0d2484457
laander <la@laander.com>
2012-04-03 17:48:21 +0200

    updated some small textual changes

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
 
+	// Replace e-mails with human readable
+	var spt = $('span.mailme');
+	var at = / at /;
+	var dot = / dot /g;
+	$(spt).each(function(i) {
+		var addr = $(this).text().replace(at,"@").replace(dot,".");
+		$(this).after('<a href="mailto:'+addr+'" title="Send an email" class="mail">'+ addr +'</a>')
+		.hover(function(){window.status="Send a letter!";}, function(){window.status="";});
+		$(this).remove();
+	});
+	
 	// Unsubscribed/removed from sign-up list
 	$(window).load(function() {
 		if (location.pathname == '/removed/') {
 			signUpResponse('<span class="message"><img src="/static/images/icon-exclamation.gif" alt="Advarsel" />Du er er blevet slettet fra tilmeldingslisten.</span>');
 			_gaq.push(['_trackEvent', 'Subscribers', 'Unsubscribe']);
-		}		
+		}
 	});
 
 	// Initialize paths
@@ -13,7 +24,6 @@ $(document).ready(function() {
 	if (path) {
 		path = path.slice(1);
 		changeContent(path);
-		console.log(path);		
 	}
 
 	// Navigation
@@ -28,29 +38,30 @@ $(document).ready(function() {
 		$('.content.visible').fadeOut(function() {
 			$(this).removeClass('visible');
 			$('.content[rel=' + id + ']').fadeIn().addClass('visible');
-		});		
+			_gaq.push(['_trackPageview', id]); // track analytics async call
+		});
 	}
 
 	// Prepare sign up inputs
 	$('form#signup input.input-email').focus(function() {
-	  var input = $(this);
-	  if (input.val() == input.attr('placeholder')) {
-	    input.val('');
-	    input.removeClass('placeholder');
-	  }
+		var input = $(this);
+		if (input.val() == input.attr('placeholder')) {
+			input.val('');
+			input.removeClass('placeholder');
+		}
 	}).blur(function() {
-	  var input = $(this);
-	  if (input.val() == '' || input.val() == input.attr('placeholder')) {
-	    input.addClass('placeholder');
-	    input.val(input.attr('placeholder'));
-	  }
+		var input = $(this);
+		if (input.val() === '' || input.val() == input.attr('placeholder')) {
+			input.addClass('placeholder');
+			input.val(input.attr('placeholder'));
+		}
 	}).blur();
 
 	// Sign up form submit
 	$('#signup').submit(function() {
 		var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 		var emailInput = $("form#signup input.input-email").val();
-		if(!emailReg.test(emailInput) || emailInput == "") {
+		if(!emailReg.test(emailInput) || emailInput === "") {
 			$('.validation-error').css('display', 'block');
 		} else {
 			$('.validation-error').hide();
@@ -58,10 +69,10 @@ $(document).ready(function() {
 			var input = $('.input-submit');
 			if (input.val() == input.attr('placeholder')) { input.val(''); }
 			$.ajax({
-			  type: 'POST',
-			  url: '/signup/',
-			  data: $("form#signup").serialize(),
-			  success: function(data){
+				type: 'POST',
+				url: '/signup/',
+				data: $("form#signup").serialize(),
+				success: function(data){
 					if (data.message == "Success") {
 						signUpResponse('<span class="message"><img src="/static/images/icon-checkmark.gif" alt="OK" />Tak for din tilmelding!</span>');
 						_gaq.push(['_trackEvent', 'Subscribers', 'Quick Form']); // track sign-up in Google Analytics
@@ -76,8 +87,8 @@ $(document).ready(function() {
 				},
 				dataType: 'json'
 			});
-		}		
-		return false;		
+		}
+		return false;
 	});
 
 	// Sign up response
@@ -85,6 +96,6 @@ $(document).ready(function() {
 		$('form#signup').fadeOut(function() {
 			$('form#signup').html(message);
 			$('form#signup').fadeIn();
-		});			
+		});
 	}
 });
